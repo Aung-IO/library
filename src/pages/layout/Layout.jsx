3144b58448e78e40d74841031094eb40a6d5cc57
Aung Pyae <57700979+Aung-IO@users.noreply.github.com>
2023-06-25 11:46:07 +0630

    transition between pages

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../../components/Navbar";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
+import './Style.css'
+
 
 export default function Layout() {
+  
+  const location = useLocation();
+  console.log(location.pathname);
+
+
   return (
     <>
       <Navbar />
+
+      <SwitchTransition>
+        <CSSTransition timeout={200} classNames='fade'  key={location.pathname}>
+
       <div className="max-w-6xl mx-auto p-6">
         <Outlet />
       </div>
+        </CSSTransition>
+      </SwitchTransition>
     </>
   );
 }
