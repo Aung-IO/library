b143e065812ee171d0300b255fd0a33434f4b1f2
Aung Pyae <57700979+Aung-IO@users.noreply.github.com>
2023-06-28 23:48:22 +0630

    add bookDetail page

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import useFetch from "../hooks/useFetch";
 import { useParams } from "react-router";
+import bookImg from "../assets/cover.png";
+import useFetch from "../hooks/useFetch";
 
 export default function BookDetail() {
   let { id } = useParams();
@@ -14,7 +15,27 @@ export default function BookDetail() {
       {error && <p>{error}</p>}
       {loading && <p>Loading...</p>}
 
-      {book && <h1>{book.title}</h1>}
+      {book && (
+        <div className="grid grid-cols-2">
+          <div>
+            <img src={bookImg} alt="" className="w-[80%]" />
+          </div>
+          <div className="space-y-4">
+            <h1 className="text-3xl font-bold">{book.title}</h1>
+            <div className="space-x-3">
+              {book.categories.map((category) => (
+                <span
+                  className="bg-blue-500 text-white rounded-full text-sm px-2 py-1"
+                  key={category}
+                >
+                  {category}
+                </span>
+              ))}
+            </div>
+            <p>{book.description}</p>
+          </div>
+        </div>
+      )}
     </>
   );
 }
