8919d703bc6a675265d855327cc92550bb0fa869
Aung Pyae <57700979+Aung-IO@users.noreply.github.com>
2023-07-03 22:06:28 +0630

    store book data on server

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,41 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import book from "../assets/cover.png";
-import useFetch from "../hooks/useFetch";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import book from '../assets/cover.png';
+import useFetch from '../hooks/useFetch';
 
 export default function BookList() {
-  let { data: books, loading, error } = useFetch("http://localhost:3000/books");
 
-  if (error) {
-    return <p>{error}</p>;
-  }
+    let { data: books, loading, error } = useFetch('http://localhost:3000/books');
 
-  return (
-    <div>
-      {loading && <p>Loading...</p>}
-      {/* Book List */}
-      {!!books && (
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 my-3">
-          {books.map((b) => (
-           <Link to={`/books/${b.id}`} key={b.id}>
-            <div className=" border border-1">
-              <img src={book} alt="" />
-              <div className="text-center space-y-2 mt-3 ">
-                <h1>{b.title}</h1>
-                <h1>{b.description}</h1>
-              </div>
-              {/* categories */}
-              <div className="mt-3 mb-2  flex flex-wrap" >
-                {b.categories.map((c) => (
-                  <span className=" mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-blue-500 " key={c}>
-                    {c}
-                  </span>
-                ))}
-              </div>
-            </div></Link>
-          ))}
+    if (error) {
+        return <p>{error}</p>
+    }
+
+    return (
+        <div>
+            {loading && <p>loading ... </p>}
+            {/* book list */}
+            {!!books && (
+                <div className='grid grid-cols-2 md:grid-cols-4 gap-4 my-3'>
+                    {books.map((b) => (
+                        <Link to={`/books/${b.id}`} key={b.id}>
+                            <div className='p-4 border border-1'>
+                                <img src={book} alt="" />
+                                <div className='text-center space-y-2 mt-3'>
+                                    <h1>{b.title}</h1>
+                                    <p>{b.description}</p>
+                                    {/* genres */}
+                                    <div className='flex flex-wrap'>
+                                        {b.categories.map((c) => (
+                                            <span key={c} className='mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-blue-500'> {c}</span>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    )
 }
