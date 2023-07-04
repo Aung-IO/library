c8229b578c29c968880f692b4f0f7e6b9040e7b9
Aung Pyae <57700979+Aung-IO@users.noreply.github.com>
2023-07-04 21:51:44 +0630

    minor bug fix in create and search

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -42,6 +42,8 @@ export default function BookList() {
                     ))}
                 </div>
             )}
+
+            {!books.length && <p className='text-center text-xl text-gray-500'>No Search Result Found</p>}
         </div>
     )
 }
