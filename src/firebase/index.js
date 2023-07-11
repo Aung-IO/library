d0e6bb8e1664060b108645545a103929472339aa
Aung Pyae <57700979+Aung-IO@users.noreply.github.com>
2023-07-11 21:37:26 +0630

    setup firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { Firestore, getFirestore } from "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC36gK5E5r6EK_ObGpZvNC02WNVRqqwUiQ",
+  authDomain: "library-app-f9891.firebaseapp.com",
+  projectId: "library-app-f9891",
+  storageBucket: "library-app-f9891.appspot.com",
+  messagingSenderId: "195200065726",
+  appId: "1:195200065726:web:175ce3e4cc558685b43eef",
+  measurementId: "G-N5Q8D6YJCR",
+};
+
+const app = initializeApp(firebaseConfig);
+
+let db = Firestore(app);
+
+export { db }
