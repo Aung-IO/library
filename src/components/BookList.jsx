2e98bc4a63289fb5a80d9e4760f32e5eb1fc5c2f
Aung Pyae <57700979+Aung-IO@users.noreply.github.com>
2023-07-18 23:56:31 +0630

    delete collection from firestore

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,30 +1,35 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, orderBy, query } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import book from "../assets/cover.png";
+import trash from "../assets/trash.svg";
 import { db } from "../firebase";
 import useTheme from "../hooks/useTheme";
-import LoadingAnimation from "./LoadingAnimation";
-
-
 
 export default function BookList() {
-
   let location = useLocation();
   let params = new URLSearchParams(location.search);
-  let search = params.get("search");
+  let search = params.get('search');
 
-  let [error, setError]  = useState("");
+  let [error, setError] = useState("");
   let [books, setBooks] = useState([]);
   let [loading, setLoading] = useState(false);
+
+  let deleteBook = (e, id) => {
+    e.preventDefault();
+    let ref = doc(db, "books", id);
+    console.log(ref);
+     deleteDoc(ref);
+    setBooks(prev => prev.filter(b => b.id !== id));
+}
 
   useEffect(function () {
     setLoading(true);
     let ref = collection(db, "books");
-    let q = query(ref, orderBy('date','desc'));
+    let q = query(ref, orderBy("date", "desc"));
     getDocs(q).then((docs) => {
       if (docs.empty) {
-        setError("Documents not found")
+        setError("Oops... Nothing Here !");
         setLoading(false);
       } else {
         let books = [];
@@ -32,13 +37,12 @@ export default function BookList() {
           let book = { id: doc.id, ...doc.data() };
           books.push(book);
         });
-        
+
         setBooks(books);
         setLoading(false);
-        setError('');
-      };
-          
-      })
+        setError("");
+      }
+    });
   }, []);
 
   if (error) {
@@ -49,47 +53,35 @@ export default function BookList() {
 
   return (
     <div>
-
-
-      {loading && <LoadingAnimation/>}
-      {/* book list */}
-
-      {!!books && (
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 my-3">
-          {books.map((b) => (
-            <Link to={`/books/${b.id}`} key={b.id}>
-              <div
-                className={`p-4 border border-1 min-h-[420px] ${
-                  isDark ? "text-white bg-dcard border-primary" : ""
-                }`}
-              >
-                <img src={book} alt="" />
-                <div className="text-center space-y-2 mt-3">
-                  <h1>{b.title}</h1>
-                  <p>{b.description}</p>
-                  {/* genres */}
-                  <div className="flex flex-wrap">
-                    {b.categories.map((c) => (
-                      <span
-                        key={c}
-                        className="mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-blue-500"
-                      >
-                        {" "}
-                        {c}
-                      </span>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div>
-      )}
-      {books && !books.length && (
-        <p className="text-center text-xl text-gray-500">
-          No Search Results Found
-        </p>
-      )}
+        {loading && <p>loading ... </p>}
+        {/* book list */}
+        {!!books && (
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-4 my-3'>
+                {books.map((b) => (
+                    <Link to={`/books/${b.id}`} key={b.id}>
+                        <div className={`p-4 border border-1 min-h-[420px] ${isDark ? 'text-white bg-dcard border-primary' : ''}`}>
+                            <img src={book} alt="" />
+                            <div className='text-center space-y-2 mt-3'>
+                                <h1>{b.title}</h1>
+                                <p>{b.description}</p>
+                                {/* genres */}
+                                <div className='flex flex-wrap justify-between items-center'>
+                                    <div>
+                                        {b.categories.map(c => (
+                                            <span key={c} className='mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-blue-500'> {c}</span>
+                                        ))}
+                                    </div>
+                                    <div onClick={(e) => deleteBook(e, b.id)} >
+                                        <img src={trash} alt=""  />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        )}
+        {books && !books.length && <p className='text-center text-xl text-gray-500'>No Search Results Found</p>}
     </div>
-  );
+)
 }
