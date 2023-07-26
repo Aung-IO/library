641be6415cbb65ea48616e6116de3326d62b2eae
Aung-IO <apkdust@gmail.com>
2023-07-26 23:54:25 +0630

    Revert "add LogOut feature"
    This reverts commit 9dccfcb3d050684704154c77c0e06befb18888f0.

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+let AuthContext = createContext();
+
+import React from 'react';
+
+export default function AuthContextProvider({children}) {
+  return (
+    <AuthContext.Provider value={{user: null}}>{children}</AuthContext.Provider>
+  )
+}
+
+export { AuthContext, AuthContextProvider };
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import AuthContextProvider from './contexts/AuthContext'
 import { ThemeContextProvider } from './contexts/ThemeContext'
 import './index.css'
 import router from './router'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <AuthContextProvider>
 <ThemeContextProvider>
     
     <RouterProvider router={router} />
 </ThemeContextProvider>
+    </AuthContextProvider>
 
 )
